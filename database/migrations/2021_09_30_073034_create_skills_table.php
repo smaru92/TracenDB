1174a3ef3e2e772efde475b04e40db9a8a62dbf9
smaru92 <koprad1@gamil.com>
2021-10-05 05:53:30 +0000

    [add] 신규 프로젝트 작성
    1. Laravel 기반 신규 프로젝트 작성

---
+++ database/migrations/2021_09_30_073034_create_skills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_ko', 40);
+            $table->string('name_jp', 40);
+            $table->tinyInteger('category');
+            $table->string('img', 255);
+						$table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
---
+++ database/migrations/2021_09_30_073118_create_people_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_ko', 30);
+            $table->string('name_jp', 30);
+            $table->integer('height');
+            $table->string('weight', 10);
+            $table->string('bust', 10);
+            $table->string('waist', 10);
+            $table->string('hip', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
